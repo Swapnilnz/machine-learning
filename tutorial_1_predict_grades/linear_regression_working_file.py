33e17fd65a306efdb1ab409a29489e8f146077e0
Swapnil <sbh94@uclive.ac.nz>
2020-08-30 13:23:27 +1200

    The correlation between a chosen datapoint (e.g. Grade 1) and the final grade is now shown graphically on a scatterplot.

--- tutorial_1_predict_grades/linear_regression_working_file.py
+++ tutorial_1_predict_grades/linear_regression_working_file.py
@@ -2,7 +2,9 @@ import pandas as pd
 import numpy as np
 import sklearn
 from sklearn import linear_model
-from sklearn.utils import shuffle
+import matplotlib.pyplot as pyplot
+import pickle
+from matplotlib import style
 
 data = pd.read_csv("student-mat.csv", sep=";")
 
@@ -12,22 +14,43 @@ predict = "G3"
 
 x = np.array(data.drop([predict], 1))
 y = np.array(data[predict])
-
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
+'''
+best = 0
+for _ in range(30):
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
 
-linear = linear_model.LinearRegression()
+    linear = linear_model.LinearRegression()
 
-linear.fit(x_train, y_train)
-accuracy = linear.score(x_test, y_test)
+    linear.fit(x_train, y_train)
+    accuracy = linear.score(x_test, y_test)
+    print(accuracy)
 
-print("Accuracy: ", accuracy * 100)
-print("-=" * 10)
+    if accuracy > best:
+        best = accuracy
+        with open("studentmodel.pickle", "wb") as f:
+            pickle.dump(linear, f) '''
+
+pickle_in = open("studentmodel.pickle", "rb")
+linear = pickle.load(pickle_in)
+
+# print("Accuracy: %{}".format(accuracy * 100))
+# print("-=" * 20)
+
+
 print("Coefficients: \n", linear.coef_)
 print("Intercept: \n", linear.intercept_)
-print("-=" * 10)
+print("-=" * 20)
 
 predictions = linear.predict(x_test)
 
 for x in range(len(predictions)):
     print("Student: {} | Prediction: {} | Actual: {} | Coefficients: {}".format(
-        x, predictions[x].round(), y_test[x], x_test[x]))
+            x, predictions[x].round(), y_test[x], x_test[x]))
+
+p = 'G1'
+style.use("ggplot")
+pyplot.scatter(data[p], data["G3"])
+pyplot.xlabel(p)
+pyplot.ylabel("Final Grade")
+pyplot.show()
